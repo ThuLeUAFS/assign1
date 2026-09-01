f5f3cbb00912484ebbeb182aa4538965edcf146d
Thu Le <ble100@uafs.edu>
2026-09-01 18:21:53 -0500

    update

--- assignOne/assignOne/Problem1.java
+++ assignOne/assignOne/Problem1.java
@@ -3,14 +3,30 @@ package assignOne;
 public class Problem1 {
 
 	public static void main(String[] args) {
-		
+		System.out.printf("%d%n", stringToInt("12345"));
 
 	}
 	
 	public static int stringToInt(String str) {
+		int sign = 1;
+		int start = 0;
+		int result = 0;
+		
+		if (str.charAt(0) == '-') {
+			sign = -1;
+			start = 1;
+		} else if (str.charAt(0)== '+') {
+			start = 1;
+		}
+		
+		for (int i = start; i < str.length(); i++) {
+			char c = str.charAt(i);
+			int digit = c - '0';
+			result = result * 10 + digit;
+		}
 		
 		
-		return 0;
+		return result * sign;
 	}
 
 }
--- assignOne/assignOne/Problem2.java
+++ assignOne/assignOne/Problem2.java
@@ -3,14 +3,57 @@ package assignOne;
 public class Problem2 {
 
 	public static void main(String[] args) {
-		
+		System.out.printf("%s%n", intToString(12345));
 
 	}
 	
 	public static String intToString(int value) {
+		if (value == 0) {
+			return "0";
+		}
 		
+		boolean negative = false;
 		
-		return null;
+		if (value < 0) {
+			negative = true;
+			value = -value;
+		}
+		
+		String result = "";
+		
+		while (value > 0) {
+			int digit = value % 10;
+			
+			if (digit == 0) {
+				result = "0" + result;
+				
+			} else if (digit == 1) {
+				result = "1" + result;
+			} else if (digit == 2) {
+				result = "2" + result;
+			} else if (digit == 3) {
+				result = "3" + result;
+		    } else if (digit == 4) {
+				result = "4" + result;
+		    } else if (digit == 5) {
+				result = "5" + result;
+		    } else if (digit == 6) {
+				result = "6" + result;
+		    } else if (digit == 7) {
+				result = "7" + result;
+		    } else if (digit == 8) {
+				result = "8" + result;
+		    } else if (digit == 9) {
+				result = "9" + result;
+		    }
+			
+			value  = value / 10;
+	}
+		if (negative) {
+			result = "-" + result;
+		}
+		
+		return result;
 	}
 
 }
